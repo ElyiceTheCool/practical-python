3410028c5f8276541b7050d07700e80107c4c756
Alyssia Bunts <alyssiabunts@gmail.com>
2023-07-25 20:46:31 -0400

    Initial solution

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,14 @@
 # bounce.py
 #
 # Exercise 1.5
+
+# Dropped from 100 m
+# Each bounce is 3/5 of the height it fell
+height = 100
+counter = 1
+
+while counter <= 10:
+    height = height * (3/5)
+    print(counter, height)
+
+    counter += 1
