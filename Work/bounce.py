260ac1587dca56a24fd8077cf9918b9b3f4f4f65
Alyssia Bunts <alyssiabunts@gmail.com>
2023-07-25 20:48:51 -0400

    rounded solution to 4 decimals

--- Work/bounce.py
+++ Work/bounce.py
@@ -2,13 +2,11 @@
 #
 # Exercise 1.5
 
-# Dropped from 100 m
-# Each bounce is 3/5 of the height it fell
 height = 100
 counter = 1
 
 while counter <= 10:
-    height = height * (3/5)
+    height = round((height * (3/5)), 4)
     print(counter, height)
 
     counter += 1
